08ea761ab05ed6846b3fb897ec582df74552049a
OguzhanAyar <oguzhanir@gmail.com>
2023-04-27 20:25:31 +0300

    Add project files.

---
+++ RestorantDeneme/Areas/Yonetici/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RestorantDeneme.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestorantDeneme.Areas.Yonetici
+{
+    [Area("Yonetici")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _context.Appusers.ToList();
+            var role = _context.Roles.ToList();
+            var userRol = _context.UserRoles.ToList();
+            foreach (var item in users)
+            {
+                var roleId = userRol.FirstOrDefault(i => i.UserId == item.Id).RoleId;
+                item.Role = role.FirstOrDefault(u => u.Id == roleId).Name;
+            }
+            return View(users);
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Appusers
+                .FirstOrDefaultAsync(m => m.Id == id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Yonetici/Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _context.Appusers.FindAsync(id);
+            _context.Appusers.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
